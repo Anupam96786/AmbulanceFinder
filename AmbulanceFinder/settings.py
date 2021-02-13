f455cb32a533c339534539c47f20b461efb6f0a1
Anupam96786 <anupam96786@gmail.com>
2021-02-13 20:26:34 +0530

    Email added

--- AmbulanceFinder/settings.py
+++ AmbulanceFinder/settings.py
@@ -127,3 +127,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, "assets"),
 ]
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.getenv('Email')
+EMAIL_HOST_PASSWORD = os.getenv('Password')
+EMAIL_USE_TLS = True
